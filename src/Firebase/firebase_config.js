b9ad3ae11ab2da47f848d86202d2ca9ea1762b15
jubayer <tanzimnahid6@gmail.com>
2024-01-07 12:23:43 +0600

    f c

--- src/Firebase/firebase_config.js
+++ src/Firebase/firebase_config.js
@@ -1,12 +1,20 @@
 import { initializeApp } from "firebase/app";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDbIEbF1l8_4v5E6Gcaec-GV1AkwD6EsyA",
+  authDomain: "webbazer-f9d07.firebaseapp.com",
+  projectId: "webbazer-f9d07",
+  storageBucket: " webbazer-f9d07.appspot.com",
+  messagingSenderId: "432278183189",
+  appId: "1:432278183189:web:cbfede10575f73636b9d1",
 };
 
 // Initialize Firebase
